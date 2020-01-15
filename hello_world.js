b106fb7cec3c6269c007c6c2eef71be7cd09d7ed
angelatto <pudri109@gmail.com>
2020-01-15 16:19:03 +0900

    param request test commit

--- hello_world.js
+++ hello_world.js
@@ -1,8 +1,18 @@
 var http = require('http');
+var url = require('url');
+
 http.createServer(function handler(req, res){
+    var parseUrl = url.parse(req.url);
+    console.log(parseUrl);
+    var querystring = require('querystring');
+
+    var parseQuery = querystring.parse(parseUrl.query, '&', '=');
+    console.log(parseQuery);
+
     res.writeHead(200, {'Content-type': 'text/html'});
-    res.write('<h1>My Server Test</h1>');
+    res.write('<h1>8080 Server Test</h1>');
     res.end('Hello world!\n');
-}).listen(1337, '127.0.0.1');
+}).listen(8080, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337');
+
